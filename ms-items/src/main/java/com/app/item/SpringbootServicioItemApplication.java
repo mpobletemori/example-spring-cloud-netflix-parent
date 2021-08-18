43c1ce5468939db348e32680fb0ba71feba27425
mpobletemori <mpobletemori@gmail.com>
2021-08-18 01:10:27 -0500

    resttemplate con ribbon para balanceo de carga

--- ms-items/src/main/java/com/app/item/SpringbootServicioItemApplication.java
+++ ms-items/src/main/java/com/app/item/SpringbootServicioItemApplication.java
@@ -2,9 +2,10 @@ package com.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 
-
+@RibbonClient(name="${app.ms-productos.id}")
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
