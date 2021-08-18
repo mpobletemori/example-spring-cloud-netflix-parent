099264161ef503af4b346e4157da1bfda7cfcecc
mpobletemori <mpobletemori@gmail.com>
2021-08-18 00:46:26 -0500

    solo openfeign

---
+++ ms-items/src/main/java/com/app/item/models/integration/ProductoOpenFeignRepository.java
@@ -0,0 +1,18 @@
+package com.app.item.models.integration;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.app.item.models.Producto;
+
+@FeignClient(name = "${app.ms-productos.id}", url = "${app.ms-productos.uri.base}")
+public interface ProductoOpenFeignRepository {
+	@GetMapping("/listar")
+	List<Producto> findAll();
+
+	@GetMapping("/ver/{id}")
+	Producto findById(@PathVariable Long id);
+}
--- ms-items/src/main/java/com/app/item/models/integration/ProductoRepositoryImpl.java
+++ ms-items/src/main/java/com/app/item/models/integration/ProductoRepositoryImpl.java
@@ -20,26 +20,20 @@ import com.app.item.models.Producto;
 @Component
 public class ProductoRepositoryImpl implements ProductoRepository {
 
-	@Value("${app.ms-productos.uri.listar}")
-	private String appMsProductosUriListar;
-	
-	@Value("${app.ms-productos.uri.ver}")
-	private String appMsProductosUriVer;
+	private ProductoOpenFeignRepository productoOpenFeignRepository;
 
-	private RestTemplate clienteRest;
-
-	public ProductoRepositoryImpl(RestTemplate clienteRest) {
-		this.clienteRest = clienteRest;
+	public ProductoRepositoryImpl(ProductoOpenFeignRepository productoOpenFeignRepository) {
+		this.productoOpenFeignRepository = productoOpenFeignRepository;
 	}
 
 	@Override
 	public List<Producto> findAll() {
-		return List.of(this.clienteRest.getForObject(this.appMsProductosUriListar, Producto[].class));
+		return this.productoOpenFeignRepository.findAll();
 	}
 
 	@Override
 	public Producto findById(Long id) {
-		return this.clienteRest.getForObject(this.appMsProductosUriVer, Producto.class, Map.of("id", id.toString()));
+		return this.productoOpenFeignRepository.findById(id);
 	}
 
 }
