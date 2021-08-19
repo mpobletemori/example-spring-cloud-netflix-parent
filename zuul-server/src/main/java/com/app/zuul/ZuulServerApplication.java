8502bf865870e9e8b339acbfcc26ae45c7459d99
mpoblete <mpoblete@vectoritcgroup.com>
2021-08-19 17:18:25 -0500

    correccion zuul inicial

--- zuul-server/src/main/java/com/app/zuul/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/app/zuul/ZuulServerApplication.java
@@ -1,4 +1,4 @@
-package com.app.eureka;
+package com.app.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 
 }
