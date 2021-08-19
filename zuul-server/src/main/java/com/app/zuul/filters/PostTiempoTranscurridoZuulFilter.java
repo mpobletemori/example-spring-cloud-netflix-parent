3464447b541c483d2c7d2fb2baab747cd77eb3ff
mpoblete <mpoblete@vectoritcgroup.com>
2021-08-19 17:49:10 -0500

    implementar zuul filtros pre y post tiempo transcurrido

---
+++ zuul-server/src/main/java/com/app/zuul/filters/PostTiempoTranscurridoZuulFilter.java
@@ -0,0 +1,48 @@
+package com.app.zuul.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class PostTiempoTranscurridoZuulFilter extends ZuulFilter {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(PostTiempoTranscurridoZuulFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		LOGGER.info("Entrando a post filter");
+		Long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
+		Long tiempoFinal = System.currentTimeMillis();
+		Long tiempoTranscurrido = tiempoFinal - tiempoInicio;
+		LOGGER.info(
+				String.format("Tiempo transcurrido en segundos %s seg.", tiempoTranscurrido.doubleValue() / 1000.00));
+		LOGGER.info(String.format("Tiempo transcurrido en milisegundos %s ms.", tiempoTranscurrido));
+
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
