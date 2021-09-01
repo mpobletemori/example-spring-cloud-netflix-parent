7cc57d59a69d31fe18f9e7924612053021b15811
mpobletemori <mpobletemori@gmail.com>
2021-08-31 20:16:19 -0500

    usar filtros de fabrica sp-cloud-gateway

--- ms-items/src/main/java/com/app/item/controllers/ItemController.java
+++ ms-items/src/main/java/com/app/item/controllers/ItemController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.item.models.Item;
@@ -21,7 +23,9 @@ public class ItemController {
 	}
 
 	@GetMapping("/listar")
-	public List<Item> listar() {
+	public List<Item> listar(@RequestParam(name="nombre",required = false) String nombre,@RequestHeader(name = "token-request",required = false) String token) {
+		System.out.println("nombre request="+nombre);
+		System.out.println("token-request="+token);
 		return this.itemService.findAll();
 	}
 
