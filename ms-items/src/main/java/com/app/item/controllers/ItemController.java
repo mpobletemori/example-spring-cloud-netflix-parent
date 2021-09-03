a4bb2b4905d665163dc7a96a95d79b21fc1b3751
mpobletemori <mpobletemori@gmail.com>
2021-09-02 23:15:03 -0500

    implementacion resilience4j-deshabilitando hystrix

--- ms-items/src/main/java/com/app/item/controllers/ItemController.java
+++ ms-items/src/main/java/com/app/item/controllers/ItemController.java
@@ -2,6 +2,7 @@ package com.app.item.controllers;
 
 import java.util.List;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,32 +12,36 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.item.models.Item;
 import com.app.item.models.Producto;
 import com.app.item.models.services.ItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
 
 	private ItemService itemService;
+	private CircuitBreakerFactory cbFactory;
 
-	public ItemController(ItemService itemService) {
+	public ItemController(ItemService itemService, CircuitBreakerFactory cbFactory) {
 		this.itemService = itemService;
+		this.cbFactory = cbFactory;
 	}
 
 	@GetMapping("/listar")
-	public List<Item> listar(@RequestParam(name="nombre",required = false) String nombre,@RequestHeader(name = "token-request",required = false) String token) {
-		System.out.println("nombre request="+nombre);
-		System.out.println("token-request="+token);
+	public List<Item> listar(@RequestParam(name = "nombre", required = false) String nombre,
+			@RequestHeader(name = "token-request", required = false) String token) {
+		System.out.println("nombre request=" + nombre);
+		System.out.println("token-request=" + token);
 		return this.itemService.findAll();
 	}
 
-	@HystrixCommand(fallbackMethod = "metodoAlternativo")
+	// @HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return this.itemService.findById(id, cantidad);
+		return this.cbFactory.create("items").run(()->this.itemService.findById(id, cantidad),e->metodoAlternativo(id, cantidad));	
 	}
-	
+
 	public Item metodoAlternativo(Long id, Integer cantidad) {
-		return Item.builder().cantidad(cantidad).producto(Producto.builder().id(id).nombre("prodError").precio(500.00).build()).build();
+		return Item.builder().cantidad(cantidad)
+				.producto(Producto.builder().id(id).nombre("prodError").precio(500.00).build()).build();
 	}
 
 }
