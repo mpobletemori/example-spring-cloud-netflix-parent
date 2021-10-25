4e7430ff6ae5de3583b1521c9fb393f80dc61302
mpobletemori <mpobletemori@gmail.com>
2021-10-25 06:05:54 -0500

    10-conf-lib-commons

--- ms-commons/src/main/java/com/app/commons/MsCommonsApplication.java
+++ ms-commons/src/main/java/com/app/commons/MsCommonsApplication.java
@@ -1,13 +1,13 @@
 package com.app.commons;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsCommonsApplication.class, args);
-	}
+//comentar por que es un jar como dependencia no un ejecutable
+//	public static void main(String[] args) {
+//		SpringApplication.run(MsCommonsApplication.class, args);
+//	}
 
 }
