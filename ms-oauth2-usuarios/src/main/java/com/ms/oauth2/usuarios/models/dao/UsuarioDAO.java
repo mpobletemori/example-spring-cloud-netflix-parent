e15ee78815bd0c8ca60ba823035bf95fbc8ee17e
mpoblete <mpoblete@vectoritcgroup.com>
2021-10-25 18:16:26 -0500

    11-personalizar metodos personalizados

--- ms-oauth2-usuarios/src/main/java/com/ms/oauth2/usuarios/models/dao/UsuarioDAO.java
+++ ms-oauth2-usuarios/src/main/java/com/ms/oauth2/usuarios/models/dao/UsuarioDAO.java
@@ -1,11 +1,14 @@
 package com.ms.oauth2.usuarios.models.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.ms.oauth2.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path="usuarios")
 public interface UsuarioDAO extends PagingAndSortingRepository<Usuario,Long>{
-    Usuario findByUsername(String username);
+	@RestResource(path = "buscar-username")
+    Usuario findByUsername(@Param("username") String username);
 }
