28c2bae314ecd6ab8c372075f19db35a432ab968
mpobletemori <mpobletemori@gmail.com>
2021-10-25 07:01:36 -0500

    11-agregar ms usuarios

---
+++ ms-oauth2-usuarios/src/main/java/com/ms/oauth2/usuarios/MsOauth2UsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.ms.oauth2.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOauth2UsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOauth2UsuariosApplication.class, args);
+	}
+
+}
---
+++ ms-oauth2-usuarios/src/test/java/com/ms/oauth2/usuarios/MsOauth2UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.oauth2.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOauth2UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
