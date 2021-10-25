2ea14217af84cb9e9615d909714dbd4007359047
mpoblete <mpoblete@vectoritcgroup.com>
2021-10-25 18:30:46 -0500

    11-crear proyecto commons user

---
+++ ms-commons-oauth2-usuarios/src/main/java/com/ms/commons/oauth2/usuarios/MsCommonsOauth2UsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.ms.commons.oauth2.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCommonsOauth2UsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCommonsOauth2UsuariosApplication.class, args);
+	}
+
+}
---
+++ ms-commons-oauth2-usuarios/src/test/java/com/ms/commons/oauth2/usuarios/MsCommonsOauth2UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.commons.oauth2.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCommonsOauth2UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
