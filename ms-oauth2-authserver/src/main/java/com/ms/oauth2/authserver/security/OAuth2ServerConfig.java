ce05b44ae885af94bc26b728d3b815694bdfaa48
mpoblete <mpoblete@vectoritcgroup.com>
2021-10-26 10:32:23 -0500

    11-Agregando config de clients en ms-oauth2

--- ms-oauth2-authserver/src/main/java/com/ms/oauth2/authserver/security/OAuth2ServerConfig.java
+++ ms-oauth2-authserver/src/main/java/com/ms/oauth2/authserver/security/OAuth2ServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -16,22 +17,43 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2ServerConfig extends AuthorizationServerConfigurerAdapter {
-//	@Autowired
-//	private BCryptPasswordEncoder passwordEncoder;
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-
+    
+	//inicio configuracion de clients
+	//Configuracion de niveles de permisos para las url de autenticacion y autorizacion (validacion de token)
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		super.configure(security);
+		security
+		.tokenKeyAccess("permitAll()")//permitir acceso a la url de generacion de token /oauth/token
+		.checkTokenAccess("isAuthenticated()");//debe ser autenticado la validacion del token
 	}
-
+    
+	//configuracion de credenciales de clients
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		super.configure(clients);
+		clients.inMemory()
+		 .withClient("frontendapp")
+		 .secret(passwordEncoder.encode("12345"))
+		 .scopes("read","write")
+		 .authorizedGrantTypes("password","refresh_token")
+		 .accessTokenValiditySeconds(3600)
+		 .refreshTokenValiditySeconds(3600)
+		 .and()
+		 .withClient("androidapp")
+		 .secret(passwordEncoder.encode("12345"))
+		 .scopes("read","write")
+		 .authorizedGrantTypes("password","refresh_token")
+		 .accessTokenValiditySeconds(3600)
+		 .refreshTokenValiditySeconds(3600);
 	}
-
+	//fin configuracion de clients
+    
+	
+	//inicio configuracion usuarios owners
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(this.authenticationManager)
@@ -50,5 +72,6 @@ public class OAuth2ServerConfig extends AuthorizationServerConfigurerAdapter {
 		jwtAccessTokenConverter.setSigningKey("algun_codigo_secreto_aeiou");
 		return jwtAccessTokenConverter;
 	}
+	//fin configuracion usuarios owners
 
 }
