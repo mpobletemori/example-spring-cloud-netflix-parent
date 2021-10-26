d07dffd4aa2e5edd471aa1eb29d02711c8be40a4
mpoblete <mpoblete@vectoritcgroup.com>
2021-10-26 06:54:16 -0500

    11-crear oauth2 authserver

---
+++ ms-oauth2-authserver/src/main/java/com/ms/oauth2/authserver/MsOauth2AuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.ms.oauth2.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOauth2AuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOauth2AuthserverApplication.class, args);
+	}
+
+}
---
+++ ms-oauth2-authserver/src/test/java/com/ms/oauth2/authserver/MsOauth2AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.oauth2.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOauth2AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
