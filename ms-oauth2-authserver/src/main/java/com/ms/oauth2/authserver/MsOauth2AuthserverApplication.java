a7c5eb8a76c6cad9a5169af19da557eff97b9f66
mpoblete <mpoblete@vectoritcgroup.com>
2021-10-26 07:10:16 -0500

    11-config oauth2 authserver

--- ms-oauth2-authserver/src/main/java/com/ms/oauth2/authserver/MsOauth2AuthserverApplication.java
+++ ms-oauth2-authserver/src/main/java/com/ms/oauth2/authserver/MsOauth2AuthserverApplication.java
@@ -2,7 +2,9 @@ package com.ms.oauth2.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsOauth2AuthserverApplication {
 
