6246f894ec942e68b4a5ac47a3f0763689fe9ef8
mpobletemori <mpobletemori@gmail.com>
2021-10-28 07:23:15 -0500

    11-manejo evento exito-fracaso autenticacion

--- ms-oauth2-authserver/src/main/java/com/ms/oauth2/authserver/security/SpringSecurityConfig.java
+++ ms-oauth2-authserver/src/main/java/com/ms/oauth2/authserver/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ms.oauth2.authserver.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,10 +16,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService usuarioServiceImpl;
 
+	@Autowired
+	private AuthenticationEventPublisher eventPublisher;
+
 	@Override
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.usuarioServiceImpl).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(this.usuarioServiceImpl).passwordEncoder(passwordEncoder()).and()
+				.authenticationEventPublisher(this.eventPublisher);
 	}
 
 	@Bean
