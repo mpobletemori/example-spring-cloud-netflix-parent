660c5a3caf36aaf7b4c774884830f1266bbd64cb
mpobletemori <mpobletemori@gmail.com>
2022-01-18 06:29:40 -0500

    16.-Configuracion de zipkin hacia mysql

--- zipkin.cmd
+++ zipkin.cmd
@@ -1,3 +1,10 @@
 @echo off
-set RABBIT_ADDRESSES=localhost:5672
+set RABBIT_ADDRESSES      =localhost:5672
+set STORAGE_TYPE          =mysql
+set MYSQL_HOST            =localhost
+set MYSQL_TCP_PORT        =3306
+set MYSQL_USER            =zipkin 
+set MYSQL_PASS            =zipkin
+set MYSQL_MAX_CONNECTIONS =10
+set MYSQL_USE_SSL         =false
 java -jar ./zipkin-server-2.23.4-exec.jar --server.port=7000
